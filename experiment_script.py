87362f714b7f6b750ad298f01d415cdd065bfc90
gzholtkevych <g.zholtkevych@karazin.ua>
2019-10-06 12:45:56 +0300

    impoving wrapped code

--- experiment_script.py
+++ experiment_script.py
@@ -5,25 +5,22 @@ from sys import exit
 from utils import *
 
 
-nvars = 2  # number of choice variants
 # ----------------------------------------------------------
 # community specification
 # ----------------------------------------------------------
 # specify community net graph
-@decorator_for_specifying_community_graph
-def define_community():
+@decorate_specifying_community_graph
+def define_community():  # returns the community graph
     # The code of this function depends on an experiment.
     # The function realises creating for the experiment
     #   the concrete graph of the network community.
-    # The created graph should be returned.
     return nx.complete_graph(200)
-net = define_community(nvars)
 # ----------------------------------------------------------
 # assign parameter values of community actors
-@decorator_for_assigning_actors_parameter_values
+@decorate_assigning_actors_parameter_values
 def assign_actors_parameter_values(
-    net  # the created community graph
-):
+    net  # the community graph
+):  # returns None
     # The code of this function depends on an experiment.
     # The function realises assigning for the experiment
     #   community actors parameter values.
@@ -32,19 +29,17 @@ def assign_actors_parameter_values(
     for n in net:
         net.nodes[n]['rho'] = 20
     net.nodes[0]['choice'] = 0
-assign_actors_parameter_values(net)
 # ----------------------------------------------------------
 # assign parameter values of community channels
-@decorator_for_assigning_channels_parameter_values
+@decorate_assigning_channels_parameter_values
 def assign_channels_parameter_values(
-    net  # the created community graph
-):
+    net  # the community graph
+):  # returns None
     # The code of this function depends on an experiment.
     # The function realises assigning for the experiment
     #   community channels parameter values.
     # Default values: a = 1.0 and D = [[0.5, 0.5],
     #                                  [0.5, 0.5]].
-    # The function returns None.
     for channel in net.edges:
         alice, _ = actor_roles(channel)
         if alice == 0:
@@ -58,111 +53,42 @@ def assign_channels_parameter_values(
                     rg.uniform(low=0.2, high=0.6),
                     rg.uniform(low=0.2, high=0.6)
                 )
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# initialize the netwoork community for studying
+# ----------------------------------------------------------
+nvars = 2                        # number of choice variants
+net = define_community(nvars)
+assign_actors_parameter_values(net)
 assign_channels_parameter_values(net)
-
-# # auxiliary parameters initialisation
-# for n in net.nodes:
-#     net.nodes[n]['result_list'] = []
-# # ----------------------------------------------------------
+# ----------------------------------------------------------
 
 
-# #
-# def simulate_dialog(alice, bob) :
-#     global net
-# # get dialogue matrix of the current dialogue and
-# # the preference densities of its participants
-#     D = net.edges[alice, bob]['D']
-#     wA = net.nodes[alice]['w']
-#     wB = net.nodes[bob]['w']
-#     wA_result = np.zeros(net.nvars)
-#     wB_result = np.zeros(net.nvars)
-#     for v in range(net.nvars):
-#         wA_result[v] = D[0, 0] * wA[v] + D[0, 1] * wB[v]
-#         wB_result[v] = D[1, 0] * wA[v] + D[1, 1] * wB[v]
-#     return wA_result, wB_result
+# ----------------------------------------------------------
+# experiment specification
+# ----------------------------------------------------------
+
+# specify initial prefernce densities of community actors
+@decorate_specifying_initial_prefernce_densities
+def specify_initial_preference_densities(
+    net  # the network community under stufying
+):  # returns None
+    # The code of this function depends on an experiment.
+    # The function realises specifying for the experiment
+    #   community actors specific preference densities.
+    # Default values: w = [1 / nvars, ..., 1 / nvars].
+    # If this is what you need then uncomment the next line
+    # pass
+    # Otherwise insert your code below
+    net.nodes[0]['w'] = np.array([1.0, 0.0], float)
+    net.nodes[1]['w'] = np.array([0.0, 1.0], float)
+# ----------------------------------------------------------
 
 
-# #
-# def simulate_session():
-#     global net
-# # clean auxiliary information
-#     for channel in net.edges:
-#         for ic in channel:
-#             net.nodes[ic]['result_list'][:] = []
-# # simulate session dialogues
-#     for channel in net.edges:
-#         if not Bernoulli_trial(net.edges[channel]['a']):
-#         # channel is not active
-#             continue
-#         else:  # channel active
-#             pass
-#     # determine actors participating as Alice and Bob in the
-#     # current dialogue
-#         alice, bob = min(channel), max(channel)
-#     # ------------------------------------------------------
-#         wA, wB = simulate_dialog(alice, bob)
-#         net.nodes[alice]['result_list'].append(wA)
-#         net.nodes[bob]['result_list'].append(wB)
-# # compute the previous session result for each community actor
-#     for n in net:
-#         if net.nodes[n]['result_list']:
-#         # actor 'n' participates at least in one dealogue
-#             ndialogues = len(net.nodes[n]['result_list'])
-#             w = np.zeros(net.nvars)
-#             for wc in net.nodes[n]['result_list']:
-#                 np.add(w, wc, w)
-#             np.multiply(w, 1.0 / ndialogues,
-#                 net.nodes[n]['w'])
-
-
-# #
-# def observation():
-# # polling simulation
-#     for n in net:
-#         hn = h(net.nodes[n]['w'])
-#         if Bernoulli_trial(
-#                 np.power(hn, net.nodes[n]['rho'])):
-#         # actor 'n' disclaims a choice
-#             net.nodes[n]['choice'] = DISCLAIMER
-#         else:
-#         # actor 'n' chooses
-#             net.nodes[n]['choice'] = np.random.choice(
-#                 net.nvars, p=net.nodes[n]['w'])
-# # compute average preference density
-#     W = np.zeros(net.nvars)
-#     for n in net:
-#         np.add(W, net.nodes[n]['w'], W)
-#     np.multiply(W, 1.0 / net.number_of_nodes(), W)
-# # compute polling result
-#     DP = len([1 for n in net
-#             if net.nodes[n]['choice'] == DISCLAIMER])
-#     if DP == net.number_of_nodes():
-#     # all community actors disclaimed a choice 
-#         return W, 1.0, uncertainty(net.nvars)
-#     NP = net.number_of_nodes() - DP
-#     WP = net.nvars * [None]
-#     for v in range(net.nvars):
-#         WP[v] = len([1 for n in net
-#             if net.nodes[n]['choice'] == v])
-#         WP[v] /= NP
-#     DP /= net.number_of_nodes()
-#     return W, DP, WP
-
-
-# # ----------------------------------------------------------
-# # experiment specification
-# # ----------------------------------------------------------
-
-# # specify initial prefernce densities of community actors
-# for n in net:
-#     if n == 0:
-#         net.nodes[n]['w'] = np.array([1.0, 0.0], float)
-#     elif n == 1:
-#         net.nodes[n]['w'] = np.array([0.0, 1.0], float)
-#     else:
-#         net.nodes[n]['w'] = uncertainty(net.nvars)
-
-# niter = 1000  # define number of iterations
+specify_initial_preference_densities(net)
+niter = 1000                          # number of iterations
 
 # # set up the experiment
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.random as rnd
+import networkx as nx
 
 
 DISCLAIMER = -1
@@ -59,7 +60,7 @@ def input_non_empty(prompt):
             print("Error!", end=' ')
 
 
-def decorator_for_specifying_community_graph(
+def decorate_specifying_community_graph(
     specifying_function
 ):
     def wrapped_function(nvars):
@@ -71,18 +72,21 @@ def decorator_for_specifying_community_graph(
     return wrapped_function
 
 
-def decorator_for_assigning_actors_parameter_values(
+def decorate_assigning_actors_parameter_values(
     assigning_function
 ):
     def wrapped_function(net):
-        for n in net:
+        for n in net:  # default assigning
             net.nodes[n]['choice'] = DISCLAIMER
             net.nodes[n]['rho'] = 1.0
+            # the list for storing auxiliary data
+            #   of the node
+            net.nodes[n]['aux_list'] = []
         assigning_function(net)
     return wrapped_function
 
 
-def decorator_for_assigning_channels_parameter_values(
+def decorate_assigning_channels_parameter_values(
     assigning_function
 ):
     def wrapped_function(net):
@@ -91,4 +95,104 @@ def decorator_for_assigning_channels_parameter_values(
             net.edges[channel]['D'] = \
                 define_dialogue_matrix(0.5, 0.5)
         assigning_function(net)
+    return wrapped_function
+
+
+def simulate_dialog(
+    net,     # a network community
+    channel  # a communication channel
+):  # returns the preference densities of the current
+    # dialogue participants after the dialogue
+    alice, bob = actor_roles(channel)
+    # get dialogue matrix of the current dialogue and
+    # the preference densities of its participants
+    # before the dialogue
+    D = net.edges[channel]['D']
+    wAlice = net.nodes[alice]['w']
+    wBob = net.nodes[bob]['w']
+    # initialisation the preference densities of the current
+    # dialogue participants after the dialogue
+    wAlice_after, wBob_after = \
+        np.zeros(net.nvars), np.zeros(net.nvars)
+    for v in range(net.nvars):
+        wAlice_after[v], wBob_after[v] = \
+            D[0, 0] * wAlice[v] + D[0, 1] * wBob[v],
+            D[1, 0] * wAlice[v] + D[1, 1] * wBob[v]
+    return wAlice_after, wBob_after
+
+
+def clear_aux_list(net):
+    for n in net:
+        net.nodes[n]['aux_list'][:] = []
+
+
+def simulate_session(
+    net  # a network community
+):  # returns None
+    clear_aux_list(net)
+    for channel in net.edges:  # simulate session dialogues
+        if Bernoulli_trial(net.edges[channel]['a']):
+        # channel is active
+            pass
+        else:  # channel is not active
+            continue
+        alice, bob = actor_roles(channel)
+        wAlice, wBob = simulate_dialog(net, channel)
+        net.nodes[alice]['aux_list'].append(wAlice)
+        net.nodes[bob]['aux_list'].append(wBob)
+    for n in net:  # compute the session results
+        if net.nodes[n]['aux_list']:
+        # actor 'n' participates at least in one dealogue
+            ndialogues = len(net.nodes[n]['aux_list'])
+            w = np.zeros(net.nvars)
+            for wc in net.nodes[n]['aux_list']:
+                np.add(w, wc, w)
+            np.multiply(w, 1.0 / ndialogues,
+                net.nodes[n]['w'])
+
+
+def observation(
+    net  # a network community
+):  # returns community preference density,
+    #         disclaimer probability, and
+    #         estimated community preference density
+    for n in net:  # polling simulation
+        hn = h(net.nodes[n]['w'])
+        if Bernoulli_trial(
+                np.power(hn, net.nodes[n]['rho'])):
+        # actor 'n' disclaims a choice
+            net.nodes[n]['choice'] = DISCLAIMER
+        else:
+        # actor 'n' chooses
+            net.nodes[n]['choice'] = \
+                np.random.choice(
+                    net.nvars, p=net.nodes[n]['w']
+                )
+    # compute community preference density
+    W = np.zeros(net.nvars)
+    for n in net:
+        np.add(W, net.nodes[n]['w'], W)
+    np.multiply(W, 1.0 / net.number_of_nodes(), W)
+    # compute polling result
+    DN = len([1 for n in net  # number of disclaiming nodes
+                if net.nodes[n]['choice'] == DISCLAIMER])
+    if DN == net.number_of_nodes():
+    # all community actors disclaimed a choice 
+        return W, 1.0, uncertainty(net.nvars)
+    NP = net.number_of_nodes() - DN
+    WP = net.nvars * [None]
+    for v in range(net.nvars):
+        WP[v] = len([1 for n in net
+                       if net.nodes[n]['choice'] == v])
+        WP[v] /= NP
+    return W, DN / net.number_of_nodes(), WP
+
+
+def decorate_specifying_initial_prefernce_densities(
+    specifying_function
+):
+    def wrapped_function(net):
+        for n in net:
+            net.nodes[n]['w'] = uncertainty(net.nvars)
+        specifying_function(net)
     return wrapped_function
